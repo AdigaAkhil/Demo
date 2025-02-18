51814c9e0558c9be5f6066714b6e92d162ef3520
akhil <akhiladiga10@gmail.com>
2025-02-18 16:27:05 +0530

    added demo route

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
+const demoRoute = require("./routes/demo.js");
+
+app.use(demoRoute);
+
 app.listen(8080, () => console.log("App running on port 8080 ..."));
