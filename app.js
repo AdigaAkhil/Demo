644334a3733f82078a4230340f2e6de86ac58865
akhil <akhiladiga10@gmail.com>
2025-02-19 21:11:58 +0530

    Minor changes in app.js and req.text

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const demoRoute = require("./routes/demo.js");
 
+const x = 12;
+
 app.use((req, res, next) => {
   console.log("This middleware will be triggered everytime!");
   console.log("Logging middleware triggered ...");
