25f6364b5b3a88f48cfe9eb22072dc176c387c4a
akhil <akhiladiga10@gmail.com>
2025-02-19 15:25:30 +0530

    Added logging middleware

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express();
 
 const demoRoute = require("./routes/demo.js");
 
+app.use((req, res, next) => {
+  console.log("Logging middleware triggered ...");
+  next();
+});
+
 app.use(demoRoute);
 
 app.listen(8080, () => console.log("App running on port 8080 ..."));
