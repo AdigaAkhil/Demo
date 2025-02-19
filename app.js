be3154557c6e31a76f55e487e54281221dedfd09
akhil <akhiladiga10@gmail.com>
2025-02-19 21:25:13 +0530

    Declared dummy variable

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const x = 12;
 const a = 12;
 const b = 55;
 
+const c = "Hello-world";
+
 app.use((req, res, next) => {
   console.log("This middleware will be triggered everytime!");
   console.log("Logging middleware triggered ...");
